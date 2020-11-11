5741ea1313a6ebbc66dbbee21b19cbf46d71ae8a
Fabricio <fabricio.jacob@outlook.com>
2020-11-11 08:48:10 -0300

    add

--- gatewayserver/src/main/java/com/fabriciolfj/github/gatewayserver/filters/TrackingFilter.java
+++ gatewayserver/src/main/java/com/fabriciolfj/github/gatewayserver/filters/TrackingFilter.java
@@ -1,5 +1,7 @@
 package com.fabriciolfj.github.gatewayserver.filters;
 
+import org.apache.commons.codec.binary.Base64;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +34,8 @@ public class TrackingFilter implements GlobalFilter {
             logger.debug("tmx-correlation-id generated in tracking filter: {}.", correlationID);
         }
 
+        System.out.println("The authentication name from the token is : " + getAuthenticationName(requestHeaders));
+
         return chain.filter(exchange);
     }
 
@@ -46,5 +50,25 @@ public class TrackingFilter implements GlobalFilter {
 
     private String generateCorrelationId() {
         return java.util.UUID.randomUUID().toString();
+    }
+
+    private String getAuthenticationName(HttpHeaders requestHeaders){
+        String authenticationName = "";
+        if (filterUtils.getAuthToken(requestHeaders)!=null){
+            String authToken = filterUtils.getAuthToken(requestHeaders).replace("Bearer ","");
+            JSONObject jsonObj = decodeJWT(authToken);
+            authenticationName = jsonObj.getString("authentication_name");
+        }
+        return authenticationName;
+    }
+
+
+    private JSONObject decodeJWT(String JWTToken) {
+        String[] split_string = JWTToken.split("\\.");
+        String base64EncodedBody = split_string[1];
+        Base64 base64Url = new Base64(true);
+        String body = new String(base64Url.decode(base64EncodedBody));
+        JSONObject jsonObj = new JSONObject(body);
+        return jsonObj;
     }
 }
